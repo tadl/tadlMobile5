54d422764a40258e2db40c3c188989ad633b2940
William Rockwood <wrockwood@tadl.org>
2019-02-25 12:14:24 -0500

    locations
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -6,19 +6,29 @@ import { format, formatDistance, formatRelative, subDays, parseISO } from 'date-
 export class Globals {
   constructor() { }
 
-  public system_short_name: string = 'TADL';
-  public catalog_host: string = 'catalog.tadl.org';
-  public catalog_api_host: string = 'https://catalog-preview.appstwo.tadl.org/';
+  /* customizable variables */
+  public catalog_host: string = 'catalog-preview.appstwo.tadl.org';
   public website_host: string = 'www.tadl.org';
-  public events_api_url: string = 'https://www.tadl.org/wp-json/tribe/events/v1/events';
-  public news_category_exclude: string = '93'; /* 93=Announcement */
-  public news_api_url: string = 'https://www.tadl.org/wp-json/wp/v2/posts';
-  public logo_url: string = 'https://www.tadl.org/logo.png'; /* redirected by nginx */
-  public square_logo_url: string = 'https://www.tadl.org/logo-clock-only.png'; /* redirected by nginx */
-  public server_error_msg: string = "Whoops. Something went wrong. Please check your internet connection and try again in a minute";
+
+  public system_short_name: string = 'TADL';
   public multi_location: boolean = true;
   public all_locations_value: string = '22';
+
+  public news_category_exclude: string = '93'; /* 93=Announcement */
+
+  public server_error_msg: string = "Whoops. Something went wrong. Please check your internet connection and try again in a minute";
   public greetings: Array<any> = ['Hello', 'Hi', 'Greetings', 'Hiya', 'Yo', 'Howdy'];
+
+  /* catalog things */
+  public catalog_api_host: string = 'https://' + this.catalog_host + '/';
+
+  /* website things */
+  public events_api_url: string = 'https://' + this.website_host + '/wp-json/tribe/events/v1/events';
+  public news_api_url: string = 'https://' + this.website_host + '/wp-json/wp/v2/posts';
+  public hours_locations_url: string = 'https://' + this.website_host + '/wp-content/uploads/json/parsed-hours.json';
+  public logo_url: string = 'https://' + this.website_host + '/logo.png'; /* redirected by nginx */
+  public square_logo_url: string = 'https://' + this.website_host + '/logo-clock-only.png'; /* redirected by nginx */
+
   public pickup_locations: Array<{name: string, code: string}> = [
     { name: 'Woodmere', code: '23' },
     { name: 'Interlochen', code: '24' },
--- src/app/locations/locations.page.ts
+++ src/app/locations/locations.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import { Globals } from '../globals';
+
+import { LoadingService } from '../services/loading/loading.service';
+import { ToastService } from '../services/toast/toast.service';
 
 @Component({
   selector: 'app-locations',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationsPage implements OnInit {
 
-  constructor() { }
+  url: string = this.globals.hours_locations_url;
+  locations: any;
+
+  constructor(
+    public globals: Globals,
+    public loading: LoadingService,
+    public toast: ToastService,
+    private http: HttpClient,
+  ) { }
+
+  get_locations() {
+
+    this.http.get(this.url)
+      .subscribe(data => {
+        if (data['locations']) {
+          this.locations = data['locations'];
+          this.loading.dismiss();
+        } else {
+          this.toast.present(this.globals.server_error_msg);
+          this.loading.dismiss();
+        }
+      }, (err) => {
+        this.toast.present(this.globals.server_error_msg);
+        this.loading.dismiss();
+      });
+
+  }
 
   ngOnInit() {
+    this.loading.present('Loading...').then(() => {
+      this.get_locations();
+    });
   }
 
 }
