9065dbbb272963e17318eb7ee23b6025498d2e4b
William Rockwood <wrockwood@tadl.org>
2019-03-13 11:12:08 -0400

    try again with device plugin
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -13,7 +13,7 @@ export class Globals {
   /* CUSTOMIZABLE VARIABLES */
 
   /* app version */
-  public app_version: string = "5.00.04";
+  public app_version: string = "5.0.5";
 
   /* basic information */
   public catalog_host: string = 'catalog-preview.appstwo.tadl.org'; /* hostname for catalog api */
