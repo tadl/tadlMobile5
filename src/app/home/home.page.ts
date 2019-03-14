8ba0bb9426f8be94c7b3c6e1b3ecc89124ed5c35
William Rockwood <wrockwood@tadl.org>
2019-03-14 14:05:21 -0400

    handle exiting app from home page differently
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Platform, MenuController } from '@ionic/angular';
+import { Platform, MenuController, AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+
 import { User } from '../user';
 import { Globals } from '../globals';
-
 
 @Component({
   selector: 'app-home',
@@ -15,14 +15,14 @@ export class HomePage {
   constructor(
     public globals: Globals,
     public user: User,
+    private alertController: AlertController,
     private menu: MenuController,
     private router: Router,
     private platform: Platform,
   ) { }
 
   query: string;
-  /*
-  subscription: any; */
+  subscription: any;
 
   search() {
     if (this.query) {
@@ -30,15 +30,31 @@ export class HomePage {
     }
   }
 
-  ionViewDidEnter() {
-    /* 
-    this.subscription = this.platform.backButton.subscribe(() => {
-      navigator['app'].exitApp();
-    }); */
+  async confirm_exit() {
+    const alert = await this.alertController.create({
+      header: 'Exit the app?',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+      }, {
+        text: 'Exit App',
+        handler: () => {
+          navigator['app'].exitApp();
+        }
+      }]
+    });
+    await alert.present();
   }
+
+  ionViewDidEnter() {
+    this.subscription = this.platform.backButton.subscribe(() => {
+      this.confirm_exit();
+    });
+  }
+
   ionViewWillLeave() {
-    /* 
-    this.subscription.unsubscribe(); */
+    this.subscription.unsubscribe();
   }
 
 }
