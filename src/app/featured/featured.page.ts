6382f8973f94dc99951e46fc01dde1b36b03b2e2
William Rockwood <wrockwood@tadl.org>
2019-03-18 07:08:56 -0400

    pre-select first item on featured page
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/featured/featured.page.ts
+++ src/app/featured/featured.page.ts
@@ -20,7 +20,7 @@ export class FeaturedPage implements OnInit {
   fmt: string = "all";
   infinite: any;
   location: string = "22";
-  media_type: any;
+  media_type: any = "Books";
   page: any;
   query: any;
   results: any;
