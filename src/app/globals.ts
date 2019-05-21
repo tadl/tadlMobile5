5e7859cd9e4dd236ee0be62889a642d00b2682bd
William Rockwood <wrockwood@tadl.org>
2019-05-21 14:34:04 -0400

    deploy build 5.1.0 update 2019051601
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -16,7 +16,7 @@ export class Globals {
 
   /* app version */
   public app_version: string = '5.1.0';
-  public update_version: string = '2019051600';
+  public update_version: string = '2019051601';
 
   /* basic information */
   public catalog_host: string = 'apiv4.catalog.tadl.org'; /* hostname for catalog api */
